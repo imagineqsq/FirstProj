c1212d081163237d9b19e250bb6a0cc561eeee74
imagineqsq <1272024200@qq.com>
2020-09-21 20:01:51 +0800

    a new version

--- app/src/main/java/com/example/firstproj/MainActivity.java
+++ app/src/main/java/com/example/firstproj/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.firstproj;
 
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -15,25 +14,49 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //this is a test to verify zzz
-//        final TextView textView = findViewById(R.id.tv2);
-//        textView.setText("hello android");
-        final TextView ttv = findViewById(R.id.ttv);
-        EditText input = findViewById(R.id.tp2);
-        String str = input.getText().toString();
-        float cc = Float.valueOf(str);
-        final double ff = cc*1.8 + 32;
-        Button bt = findViewById(R.id.button3);
-        bt.setOnClickListener(new View.OnClickListener() {
+
+        Button bt1 = (Button)findViewById(R.id.bt1);
+        Button bt2 = (Button)findViewById(R.id.bt2);
+        Button bt3 = (Button)findViewById(R.id.bt3);
+        Button bt4 = (Button)findViewById(R.id.bt4);
+        final EditText num = (EditText)findViewById(R.id.num);
+
+
+        bt1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                ttv.setText(String.valueOf(ff));
+                int score = Integer.parseInt(String.valueOf(num.getText()));
+                String scores = String.valueOf(score+3);
+                num.setText(scores);
             }
         });
 
-//        Log.i("login", "onCreate");
+        bt2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int score = Integer.parseInt(String.valueOf(num.getText()));
+                String scores = String.valueOf(score+2);
+                num.setText(scores);
+            }
+        });
 
+        bt3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int score = Integer.parseInt(String.valueOf(num.getText()));
+                String scores = String.valueOf(score+1);
+                num.setText(scores);
+            }
+        });
+
+        bt4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                num.setText("0");
+            }
+        });
 
     }
+
+
 }
