c5ce67332d66a35271667d278edea6eaf1e57a9f
imagineqsq <1272024200@qq.com>
2020-10-27 19:48:42 +0800

    get data from database

--- app/src/main/java/com/example/firstproj/MainActivity.java
+++ app/src/main/java/com/example/firstproj/MainActivity.java
@@ -20,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.transfer);
+
     }
 
     public void config(View view){
