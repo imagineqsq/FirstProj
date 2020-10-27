b5af9d76a383e454d4148ff312c6c4fdffb9a3e8
imagineqsq <1272024200@qq.com>
2020-10-27 19:50:14 +0800

    test

---
+++ app/src/main/java/com/example/firstproj/NewList.java
@@ -0,0 +1,41 @@
+package com.example.firstproj;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.SimpleAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class NewList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_list);
+
+        List listItems = new ArrayList<HashMap<String,String>>();
+        for(int i = 0; i < 10; i ++){
+            HashMap<String,String> map = new HashMap<String, String>();
+            map.put("ItemTitle","Rate: "+i);
+            map.put("ItemDetail","detail"+i);
+            listItems.add(map);
+        }
+
+        Adapter listItemAdapter = new SimpleAdapter(this,listItems,R.layout.activity_new_list,
+                new String[]{"ItemTitle","ItemDetail"}, new int[]{R.id.itemTitle,R.id.itemDetail});
+
+    }
+
+
+}
---
+++ app/src/main/java/com/example/firstproj/test1.java
@@ -0,0 +1,39 @@
+package com.example.firstproj;
+
+import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class test1 extends AppCompatActivity {
+    private String[] names = new String[]{"A","B","C","D"};
+    private String[] says = new String[]{"111","222","333","444"};
+    private int[] imgIds = new int[]{R.mipmap.birth,R.mipmap.birth,R.mipmap.birth,R.mipmap.birth};
+
+    protected void onCreate(Bundle saveInstanceState) {
+
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.test1);
+
+        List<Map<String,Object>> listitem = new ArrayList<Map<String,Object>>();
+        for(int i=0;i<names.length;i++){
+            Map<String, Object> showitem = new HashMap<String, Object>();
+            showitem.put("touxiang",imgIds[i]);
+            showitem.put("name",names[i]);
+            showitem.put("says",says[i]);
+            listitem.add(showitem);
+        }
+
+        SimpleAdapter myAdapter = new SimpleAdapter(getApplicationContext(),listitem,R.layout.test1,
+                new String[]{"touxiang","name","says"},new int[]{R.id.imagtou,R.id.name,R.id.says});
+        ListView listView = (ListView) findViewById(R.id.ls);
+        listView.setAdapter(myAdapter);
+
+    }
+}
