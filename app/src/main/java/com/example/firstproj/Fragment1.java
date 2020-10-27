de7f824b4d5684848a8a967a7dd37506a3fea2d6
imagineqsq <1272024200@qq.com>
2020-10-27 19:49:03 +0800

    fragment

---
+++ app/src/main/java/com/example/firstproj/Fragment1.java
@@ -0,0 +1,24 @@
+package com.example.firstproj;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.fragment.app.Fragment;
+
+public class Fragment1 extends Fragment {
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState){
+        return inflater.inflate(R.layout.fragment1,null);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState){
+        super.onActivityCreated(savedInstanceState);
+        TextView textView = getView().findViewById(R.id.home);
+        textView.setText("this is 主页面");
+    }
+}
---
+++ app/src/main/java/com/example/firstproj/Fragment2.java
@@ -0,0 +1,24 @@
+package com.example.firstproj;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.fragment.app.Fragment;
+
+public class Fragment2 extends Fragment {
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState){
+        return inflater.inflate(R.layout.fragment2,null);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState){
+        super.onActivityCreated(savedInstanceState);
+        TextView textView = getView().findViewById(R.id.info);
+        textView.setText("this is 信息页面");
+    }
+}
