fbb8f002a0c9ddad23c5452dd9741b2cb04d0d56
imagineqsq <1272024200@qq.com>
2020-10-27 19:41:37 +0800

    get data from database

---
+++ app/src/main/java/com/example/firstproj/Config.java
@@ -0,0 +1,25 @@
+package com.example.firstproj;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class Config extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_config);
+
+    }
+    public void Save(){
+        EditText text1 = findViewById(R.id.text1);
+        EditText text2 = findViewById(R.id.text2);
+        EditText text3 = findViewById(R.id.text3);
+        Button save = findViewById(R.id.save);
+
+
+    }
+}
--- app/src/main/java/com/example/firstproj/MainActivity.java
+++ app/src/main/java/com/example/firstproj/MainActivity.java
@@ -1,11 +1,17 @@
 package com.example.firstproj;
 
 
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,49 +19,78 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.transfer);
+    }
 
-        Button bt1 = (Button)findViewById(R.id.bt1);
-        Button bt2 = (Button)findViewById(R.id.bt2);
-        Button bt3 = (Button)findViewById(R.id.bt3);
-        Button bt4 = (Button)findViewById(R.id.bt4);
-        final EditText num = (EditText)findViewById(R.id.num);
+    public void config(View view){
+
+        Intent act2 = new Intent(this,Config.class);
+        startActivity(act2);
+
+        SharedPreferences sharedPreferences = getSharedPreferences("myrate",Activity.MODE_PRIVATE);
+        PreferenceManager.getDefaultSharedPreferences(this);
+
+        Float dollarRate = sharedPreferences.getFloat("dollar_rate",0.0f);
+        Float euroRate = sharedPreferences.getFloat("euro_rate",0.0f);
+        Float wonRate = sharedPreferences.getFloat("won_rate",0.0f);
+
+        SharedPreferences sp = getSharedPreferences("myrate",Activity.MODE_PRIVATE);
+
+        SharedPreferences.Editor editor = sp.edit();
+        editor.putFloat("dollar_rate",dollarRate);
+        editor.putFloat("euro_rate",euroRate);
+        editor.putFloat("won_rate",wonRate);
+        editor.apply();
+    }
+
+    static double usd = 6.81;//美元汇率
+    static double eu = 7.95;//欧元汇率
+    static double kr = 0.0058;//韩元汇率
 
 
-        bt1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                int score = Integer.parseInt(String.valueOf(num.getText()));
-                String scores = String.valueOf(score+3);
-                num.setText(scores);
-            }
-        });
 
-        bt2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                int score = Integer.parseInt(String.valueOf(num.getText()));
-                String scores = String.valueOf(score+2);
-                num.setText(scores);
-            }
-        });
 
-        bt3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                int score = Integer.parseInt(String.valueOf(num.getText()));
-                String scores = String.valueOf(score+1);
-                num.setText(scores);
-            }
-        });
+    public void rmb2usd(View view){
+        EditText input = (EditText)findViewById(R.id.input);
+        Button dollar_rate = (Button)findViewById(R.id.dollar);
+        String ss = input.getText().toString();
+        TextView output = (TextView)findViewById(R.id.output);
+        if(ss.equals("")){
+            Toast.makeText(this, "请输入rmb", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            Double out = Double.valueOf(ss) / usd;
+            output.setText(out.toString() + "美元");
+        }
+    }
 
-        bt4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                num.setText("0");
-            }
-        });
+    public void rmb2euro(View view){
+        EditText input = (EditText)findViewById(R.id.input);
+        Button euro_rate = (Button)findViewById(R.id.euro);
+        String ss = input.getText().toString();
+        TextView output = (TextView)findViewById(R.id.output);
+        if(ss.equals("")){
+            Toast.makeText(this, "请输入rmb", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            Double out = Double.valueOf(ss) / eu;
+            output.setText(out.toString() + "欧元");
+        }
+    }
 
+    public void rmb2won(View view){
+        EditText input = (EditText)findViewById(R.id.input);
+        Button won_rate = (Button)findViewById(R.id.won);
+        Button config = (Button)findViewById(R.id.config);
+        String ss = input.getText().toString();
+        TextView output = (TextView)findViewById(R.id.output);
+        if(ss.equals("")){
+            Toast.makeText(this, "请输入rmb", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            Double out = Double.valueOf(ss) / kr;
+            output.setText(out.toString() + "韩元");
+        }
     }
 
 
