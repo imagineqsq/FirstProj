6a8d635a26fe2da8f93c106da2d4f97ba4070f38
imagineqsq <1272024200@qq.com>
2020-10-27 19:46:09 +0800

    get data from database

---
+++ app/src/main/java/com/example/firstproj/DBHelper.java
@@ -0,0 +1,42 @@
+package com.example.firstproj;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import org.w3c.dom.Text;
+
+
+public class DBHelper extends SQLiteOpenHelper {
+
+    private static final int VERSION = 1;
+    private static final String DB_NAME = "myrate.db";
+    public static final String TB_NAME = "tb_rates";
+    Text Name;
+    Text Rate;
+
+    public DBHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    public DBHelper(Context context) {
+        super(context,DB_NAME,null,VERSION);
+    }
+
+    public void RateItem(Text getCurName,Text getCurRate){
+        Name = getCurName;
+        Rate = getCurName;
+
+
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE "+TB_NAME+"(ID INTEGER PRIMARY KEY AUTOINCREMENT,CURNAME TEXT,CURRATE TEXT)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
