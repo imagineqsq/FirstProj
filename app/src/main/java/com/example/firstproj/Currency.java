658c01100150a4d401ea9a8e50eab2bfd3efea64
imagineqsq <1272024200@qq.com>
2020-10-27 19:45:54 +0800

    get data from database

---
+++ app/src/main/java/com/example/firstproj/Currency.java
@@ -0,0 +1,31 @@
+package com.example.firstproj;
+
+public class Currency {
+    private int id;
+    private String name;
+    private String rate;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRate() {
+        return rate;
+    }
+
+    public void setRate(String rate) {
+        this.rate = rate;
+    }
+}
